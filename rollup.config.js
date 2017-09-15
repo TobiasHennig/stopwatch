ba1f5be7dca56e073a08ffcec74dfd8af225d091
Tobias Hennig <tobias.hennig1@gmail.com>
2017-09-15 11:51:34 +0200

    Closes #1
    Update build step to uglify with rollup and show filesize of minified bundle.

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,17 @@
 import babel from 'rollup-plugin-babel';
+import filesize from 'rollup-plugin-filesize';
+import uglify from 'rollup-plugin-uglify';
 
-export default {
+export default [{
   dest: 'dist/StopWatch.js',
   entry: 'src/StopWatch.js',
   format: 'umd',
   moduleName: 'StopWatch',
   plugins: [babel()]
-};
+}, {
+  dest: 'dist/StopWatch.min.js',
+  entry: 'src/StopWatch.js',
+  format: 'umd',
+  moduleName: 'StopWatch',
+  plugins: [babel(), uglify(), filesize()]
+}];
